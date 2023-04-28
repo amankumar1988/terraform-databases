d542914c4d2b2f76f7ac99fc45b5110370753a2b
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 19:47:50 +0530

    Auto Push

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "docdb" {
 }
 # This always goes with the main branch, so how can I tell the module to pick the code from intended branch
 
-# module "redis" {
-#   source                 = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
-#   ENV                    = var.ENV
-# }
+module "redis" {
+  source                 = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                    = var.ENV
+}
