3ce680f86a84b1f0af8869662218086069b5fc74
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 23:16:38 +0530

    Auto Push

--- vars.tf
+++ vars.tf
@@ -16,5 +16,5 @@ variable "DOCDB_INSTANCE_COUNT" {}
 # Redis Variable
 variable "ELASTIC_CACHE_PORT" {}
 variable "ELASTIC_CACHE_INSTANCE_TYPE" {}
-variable "ELASTIC_CACHE_NODE_COUNT " {}
+variable "ELASTIC_CACHE_NODE_COUNT" {}
 variabdata "ELASTIC_CACHE_ENGINE_VERSION" {} 
