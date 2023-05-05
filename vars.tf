8e6ce34e9bdae6d6a6e575eca811dfcc434aca9b
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 10:35:02 +0530

    Auto Push

--- vars.tf
+++ vars.tf
@@ -13,6 +13,6 @@ variable "DOCDB_INSTANCE_COUNT" {}
 
 # Elasticcache Variables 
 variable "ELASTIC_CACHE_PORT" {}
-variable "ELASTIC_CACHE_NODE_TYPE" {}
+variable "ELASTIC_CACHE_INSTANCE_TYPE" {}
 variable "ELASTIC_CACHE_NODE_COUNT" {}
 variable "ELASTIC_CACHE_ENGINE_VERSION" {}
