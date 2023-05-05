3ffd30978338f0836b155e1542ed2684ff88e6be
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 10:00:41 +0530

    Auto Push

--- vars.tf
+++ vars.tf
@@ -1,20 +1,18 @@
 variable "ENV" {}
 
-# Mysql variables
+# MySQL Variables
 variable "MYSQL_RDS_PORT" {}
 variable "MYSQL_RDS_STORAGE" {}
-variable "MYSQL_RDS_VERSION" {}
+variable "MYSQL_RDS_ENGINE_VERSION" {}
 variable "MYSQL_RDS_INSTANCE_TYPE" {}
 
-# DOCDB variables
-
+# DocDB Variables 
 variable "DOCDB_PORT" {}
 variable "DOCDB_INSTANCE_CLASS" {}
 variable "DOCDB_INSTANCE_COUNT" {}
 
-
-# Redis Variable
+# Elasticcache Variables 
 variable "ELASTIC_CACHE_PORT" {}
-variable "ELASTIC_CACHE_INSTANCE_TYPE" {}
+variable "ELASTIC_CACHE_NODE_TYPE" {}
 variable "ELASTIC_CACHE_NODE_COUNT" {}
-variable "ELASTIC_CACHE_ENGINE_VERSION" {} 
+variable "ELASTIC_CACHE_ENGINE_VERSION" {}
